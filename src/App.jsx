94bccb49f00a77affcc5cfbb891568fec87b9691
michael Means <colemeans51@gmail.com>
2022-04-04 15:54:22 -0500

    reverting

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,14 @@
-import { useContext } from "react";
 import Intro from "./components/intro/Intro"
 import About from "./components/about/About"
 import MyworkList from "./components/MyworkList/MyworkList";
 import Contactme from "./components/contact/contact";
-import Toggle from "./components/toggle/toggle"
-import { ThemeContext } from "./context";
 const App = () => {
-    const theme = useContext(ThemeContext);
-    const darkMode = theme.state.darkMode;
-    return (
-      <div
-        style={{
-          backgroundColor: darkMode ? "#222" : "white",
-          color: darkMode && "white",
-        }}
-      >
-        <Toggle />
+    return <div>
         <Intro/>
         <About/>
         <MyworkList/>
         <Contactme/>
-        </div>
-  );
+    </div>;
 };
 
 export default App;
--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,27 +3,27 @@ import me from "../../img/hiking-removebg-preview.png"
 const Intro = () => {
   return (
     <div className="i">
-      <div className="left">
-        <div className="left-wrapper">
-          <h2 className="intro">hello my name is</h2>
-          <h1 className="name">Michael</h1>
-          <div className="title">
-            <p className="ititle-wrapper">
-              <div className="title-item">Web Developer</div>
-              <div className="title-item">Musician</div>
-              <div className="title-item">Diesel Tech</div>
-              <div className="title-item">2x Bowling Champion</div>
+      <div className="i-left">
+        <div className="i-left-wrapper">
+          <h2 className="i-intro">hello my name is</h2>
+          <h1 className="i-name">Michael</h1>
+          <div className="i-title">
+            <p className="i-title-wrapper">
+              <div className="i-title-item">Web Developer</div>
+              <div className="i-title-item">Musician</div>
+              <div className="i-title-item">Diesel Tech</div>
+              <div className="i-title-item">2x Bowling League Champion</div>
               
              </p>
           </div>
-          <div className="description">
+          <div className="i-description">
               ABOUT ME GOES HERE
           </div>
         </div>
       </div>
-      <div className="iright">
+      <div className="i-right">
           <div className="i-bg"></div>
-          <img src={me} alt="" className="img" />
+          <img src={me} alt="" className="i-img" />
       </div>
     </div>
   );
