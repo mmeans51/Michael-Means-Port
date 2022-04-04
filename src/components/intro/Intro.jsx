d47d650e2549c72cae7af67ef1527d3b6adc5a9e
michael Means <colemeans51@gmail.com>
2022-04-04 15:59:50 -0500

    reverting

--- src/components/intro/Intro.jsx
+++ src/components/intro/Intro.jsx
@@ -3,6 +3,7 @@ import me from "../../img/hiking-removebg-preview.png"
 const Intro = () => {
   return (
     <div className="i">
+<<<<<<< HEAD
       <div className="i-left">
         <div className="i-left-wrapper">
           <h2 className="i-intro">hello my name is</h2>
@@ -17,6 +18,22 @@ const Intro = () => {
              </p>
           </div>
           <div className="i-description">
+=======
+      <div className="left">
+        <div className="left-wrapper">
+          <h2 className="intro">hello my name is</h2>
+          <h1 className="name">Michael</h1>
+          <div className="title">
+            <p className="ititle-wrapper">
+              <div className="title-item">Web Developer</div>
+              <div className="title-item">Musician</div>
+              <div className="title-item">Diesel Tech</div>
+              <div className="title-item">2x Bowling Champion</div>
+              
+             </p>
+          </div>
+          <div className="description">
+>>>>>>> parent of 89d2f59 (adding about me and descriptions)
               ABOUT ME GOES HERE
           </div>
         </div>
